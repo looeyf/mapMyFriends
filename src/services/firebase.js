8ec7ad4ebc3a9cee9a4b24942c522cb1d8e1053f
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2021-06-23 21:03:44 -0300

    Variaveis de ambiente

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase';
 import 'firebase/firebase-database';
 
+import config from '../../config'
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyBMa3kJZTdktQOv1pToDVoTQE1F7foslmU',
-  authDomain: 'mapmyfriends-7f3b3.firebaseapp.com',
-  databaseURL: 'https://mapmyfriends-7f3b3.firebaseio.com',
-  projectId: 'mapmyfriends-7f3b3',
-  storageBucket: 'mapmyfriends-7f3b3.appspot.com',
-  messagingSenderId: '164203873364',
-  appId: '1:164203873364:web:2e278ddd9d9316a7dd59c1',
+  apiKey: config.API_KEY,
+  authDomain: config.AUTH_DOMAIN,
+  databaseURL: config.DATABASE_URL,
+  projectId: config.PROJECT_ID,
+  storageBucket: config.STORAGE_BUCKET,
+  messagingSenderId: config.MESSAGING_SENDER_ID,
+  appId: config.APP_ID,
 };
 
 // Initialize Firebase
